3fd42b1698e73259710de9fe08b907a53fed313f
Davideuni06 <davide.dicostanzo2@studenti.unina.it>
2026-06-03 14:13:27 +0200

    Update ConnessioneDatabase.java

--- src/main/java/database_connection/ConnessioneDatabase.java
+++ src/main/java/database_connection/ConnessioneDatabase.java
@@ -1,8 +1,31 @@
 package database_connection;
 
-/**
- * The type Connessione database.
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnessioneDatabase {
 
+    private static ConnessioneDatabase instance;
+    public Connection connection;
+
+    private static final String URL      = "jdbc:postgresql://localhost:5432/gestione_lauree";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    private ConnessioneDatabase() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+            this.connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Driver PostgreSQL non trovato.", e);
+        }
+    }
+
+    public static ConnessioneDatabase getInstance() throws SQLException {
+        if (instance == null || instance.connection.isClosed()) {
+            instance = new ConnessioneDatabase();
+        }
+        return instance;
+    }
 }
